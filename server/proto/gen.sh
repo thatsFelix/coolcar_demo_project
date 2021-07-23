4d16fd157f60f2ddc8012a1c72faab48034c019d
HiFelix <741883227@qq.com>
2021-07-23 10:27:22 +0800

    re init project

--- server/proto/gen.sh
+++
@@ -1,9 +0,0 @@
-protoc -I=. --go_out=paths=source_relative:gen/go trip.proto
-protoc -I=. --go-grpc_out=paths=source_relative:gen/go trip.proto
-protoc -I=. --grpc-gateway_out=paths=source_relative,grpc_api_configuration=trip.yaml:gen/go trip.proto
-
-PBTS_BIN_DIR=../../wx/node_modules/.bin
-PBTS_OUT_DIR=../../wx/miniprogram/service/proto_gen
-
-$PBTS_BIN_DIR/pbjs -t static -w es6 trip.proto --no-create --no-encode --no-decode --no-verify --no-delimited --force-number -o $PBTS_OUT_DIR/trip_pb.js
-$PBTS_BIN_DIR/pbts -o $PBTS_OUT_DIR/trip_pb.d.ts $PBTS_OUT_DIR/trip_pb.js
