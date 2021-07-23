f954e43cc3c2701938749aac64249bc80b889eb4
HiFelix <741883227@qq.com>
2021-07-23 10:21:57 +0800

    backend code backup

--- server/proto/gen.sh
+++ server/proto/gen.sh
@@ -2,7 +2,7 @@ protoc -I=. --go_out=paths=source_relative:gen/go trip.proto
 protoc -I=. --go-grpc_out=paths=source_relative:gen/go trip.proto
 protoc -I=. --grpc-gateway_out=paths=source_relative,grpc_api_configuration=trip.yaml:gen/go trip.proto
 
-PBTS_BIN_DIR=../../wx/miniprogram/node_modules/.bin
+PBTS_BIN_DIR=../../wx/node_modules/.bin
 PBTS_OUT_DIR=../../wx/miniprogram/service/proto_gen
 
 $PBTS_BIN_DIR/pbjs -t static -w es6 trip.proto --no-create --no-encode --no-decode --no-verify --no-delimited --force-number -o $PBTS_OUT_DIR/trip_pb.js
